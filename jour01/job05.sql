c90ba7db5d5c5e3aa1e5e3a5e41ab9e0c5e70d1b
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-29 22:45:35 +0100

    Job05

---
+++ jour01/job05.sql
@@ -0,0 +1,6 @@
+INSERT INTO etudiant(nom,prenom,age,email) VALUES
+('Spaghetti','Betty', 23,'betty.Spaghetti@laplateforme.io'),
+('Steak','Chuck', 45,'chuck.steak@laplateforme.io'),
+('Doe','John', 18,'john.doe@laplateforme.io'),
+('Barnes','Binkie', 16,'binkie.barnes@laplateforme.io'),
+('Gertrude','Dupuis', 16,'gertrude.dupuis@laplateforme.io');
