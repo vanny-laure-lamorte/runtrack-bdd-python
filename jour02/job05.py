78c3d770ac493299e24c8b968c9af24f42a35200
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-30 11:07:34 +0100

    Job05

---
+++ jour02/job05.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+bdd = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "VannyLamorte25!",
+    database = "LaPlateforme",
+)
+
+cursor = bdd.cursor()
+
+cursor.execute("SELECT SUM(superficie) FROM etage")
+superficie_totale = cursor.fetchone()[0]
+
+print(f'La superficie de La Plateforme est de {superficie_totale}m2')
+
+cursor.close()
+bdd.close()
