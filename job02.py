2930f5c75e455e60836ee5e7752acc4ca3d9ac8c
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-30 10:07:24 +0100

    Job01

--- job02.py
+++ job02.py
@@ -3,11 +3,6 @@ import mysql.connector
 bdd = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "XXX",
+    password = "VannyLamorte25!",
     database = "LaPlateforme",
 )
-
-cursor = bdd.cursor()
-cursor.execute("SELECT * FROM etudiant")
-result = cursor.fetchall()
-print(result)
