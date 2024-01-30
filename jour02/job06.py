004b8ac001d1a9130488ae8eef93e7c309a32589
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-30 11:12:19 +0100

    Job06

---
+++ jour02/job06.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+bdd = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "XXX",
+    database = "LaPlateforme",
+)
+
+cursor = bdd.cursor()
+
+cursor.execute("SELECT SUM(capacite) FROM salle")
+capacite_totale = cursor.fetchone()[0]
+
+print(f'La capacite de toutes les salles est de :{capacite_totale}')
+
+cursor.close()
