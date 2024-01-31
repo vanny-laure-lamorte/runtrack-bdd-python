8616e08d6e10ed3f0b3e6436e944ec62adc2f7a4
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-01-31 09:42:14 +0100

    Job08

--- jour02/Job08.py
+++ jour02/Job08.py
@@ -1,57 +1,74 @@
-"""
+import mysql.connector
 
-CREATE DATABASE Zoo;
-SHOW Databases;
+class ZooManager:
+    def __init__(self, host, user, password, database):
+        self.connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        self.cursor = self.connection.cursor()
 
-USE Zoo;
+    def ajout_animal(self, nom, race, id_type_cage, date_naissance, pays_origine):
+        sql = "INSERT INTO animal (nom, race, id_type_cage, date_naissance, pays_origine) VALUES (%s, %s, %s, %s, %s)"
+        values = (nom, race, id_type_cage, date_naissance, pays_origine)
+        self.cursor.execute(sql, values)
+        self.connection.commit()
 
-CREATE TABLE animal(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(255),
-    race VARCHAR(255),
-    id_type_cage INT,
-    date_naissance DATE,
-    pays_origine VARCHAR(255) 
-    );
+    def supprimer_animal(self, animal_id):
+        sql = "DELETE FROM animal WHERE id = %s"
+        values = (animal_id)
+        self.cursor.execute(sql, values)
+        self.connection.commit()
 
-SHOW COLUMNS
-FROM employe;
+    def modifer_animal(self, animal_id, new_values):
+        set_clause = ", ".join([f"{key} = '{value}'" for key, value in new_values.items()])
+        sql = f"UPDATE animal SET {set_clause} WHERE id = %s"
+        self.cursor.execute(sql, (animal_id,))
+        self.connection.commit()
 
-INSERT INTO employe(nom,prenom,salaire,id_service) VALUES
-('Lorquet', 'Ines', 3800, 1),
-('Madec','Lucy', 4000, 2),
-('Iribaren','Lucas', 4800, 3),
-('Martinie','Lucas', 5000, 4),
-('Cordial', 'Alicia', 6000, 5);
+    def display_animals(self):
+        sql = "SELECT * FROM animal"
+        self.cursor.execute(sql)
+        animals = self.cursor.fetchall()
+        for animal in animals:
+            print(animal)
+            
+    def display_cage(self):
+        sql = "SELECT * FROM cage"
+        self.cursor.execute(sql)
+        cages = self.cursor.fetchall()
+        for cage in cages:
+            print(cage)
 
-SELECT *
-FROM employe;
+    def display_animals_in_cages(self):
+        sql = "SELECT cage.id, cage.superficie, cage.capacite FROM cage LEFT JOIN animal ON cage.id = animal.id_type_cage"
+        self.cursor.execute(sql)
+        animals_in_cages = self.cursor.fetchall()
+        print("Il y a ",len(animals_in_cages)," animaux dans la cage")
+        
+    def calculate_total_cage_area(self):
+        sql = "SELECT SUM(superficie) FROM cage"
+        self.cursor.execute(sql)
+        total_area = self.cursor.fetchone()[0]
+        print(f"La superficie totale de toutes les cages est de : {total_area} m²")
+        
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
 
-CREATE TABLE service (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(255)
-    ); 
+zoo_manager = ZooManager("localhost", "root", "XXX", "Zoo")
 
-SHOW COLUMNS
-FROM service;
+# zoo_manager.ajout_animal("Serpent", "Anaconda", 1, "2021-10-15", "Asie")
 
-INSERT INTO service(nom) VALUES
-('technicien de surface'),
-('Assistant Administratif'), 
-('Accompagnateur'),
-('Responsable Pedagogique'), 
-('Directeur'); 
+# zoo_manager.modifer_animal(1, {'nom': 'Lion King'})
 
-SELECT *
-FROM service; 
+# zoo_manager.supprimer_animal(2)
 
-SELECT employe.nom, employe.prenom, employe.salaire, service.nom as service
-FROM employe
-JOIN service ON employe.id_service = service.id
+# zoo_manager.display_animals()
+# zoo_manager.display_cage()
 
-"""
-
-class Salaire: 
-
-    def __init__(self):
-        pass
+zoo_manager.display_animals_in_cages()
+zoo_manager.calculate_total_cage_area()
+zoo_manager.close_connection()
